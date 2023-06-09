3d8a58add795405c38019f0728980b0551993597
Hieu Pham <hieunucek6@gmail.com>
2023-06-09 16:13:40 +0700

    Initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+console.log('%c hello ', 'color: red;')
+</script>
+
+<template>
+  <div>
+    hello
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/hello/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div />
+</template>
+
+<style scoped>
+
+</style>
+
+<route lang="yaml">
+meta:
+  layout: home
+  </route>
